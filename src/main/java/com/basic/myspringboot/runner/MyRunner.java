4656287f5c0187ab8e9f9a7b800202347a414014
rhgusdn0708 <hu070821@gmail.com>
2025-04-30 15:59:27 +0900

    logs

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,8 +1,12 @@
 package com.basic.myspringboot.runner;
 
+import com.basic.myspringboot.config.CustomerVO;
+import com.basic.myspringboot.property.MyBootProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,10 +17,29 @@ public class MyRunner implements ApplicationRunner {
     @Value("${myboot.age}")
     private int age;
 
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private MyBootProperties properties;
+
+    @Autowired
+    private CustomerVO customerVO;
+
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("${myboot.name} = " + name);
         System.out.println("${myboot.age} = " + age);
+        System.out.println("${myboot.fullName} = " + environment.getProperty("myboot.fullName"));
+
+        System.out.println("MyBootProperties getName()" + properties.getName());
+        System.out.println("MyBootProperties getAge()" + properties.getAge());
+        System.out.println("MyBootProperties getFullName()" + properties.getFullName());
+        System.out.println("설정된 Port 번호  " + environment.getProperty("local.server.port"));
+
+        System.out.println("현재 활성화된 CustomerVO Bean " + customerVO);
+
 
         // foo 라는 VM 아규먼트 있는지 확인
         System.out.println("VM 아규먼트 foo : " + args.containsOption("foo"));
